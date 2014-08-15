872ff631b478f7c40d4c2c06a883ffd59d3858ad
diek <diek@bellaliant.net>
2014-08-14 23:19:33 -0230

    put code into function, added selection for music

--- schedule_break.py
+++ schedule_break.py
@@ -1,10 +1,17 @@
 import webbrowser
 import time
 
-count = 0
-print "Current time " + time.ctime()
-while count < 3:
-    time.sleep(2)
-    print count
-    # webbrowser.open("https://www.youtube.com/watch?v=20S_kwNb4rg")
-    count += 1
+
+def take_break(num_breaks, tunes):
+    count = 0
+    print "Current time " + time.ctime()
+    while count < num_breaks:
+        time.sleep(2*60*60)
+        webbrowser.open(tunes[count])
+        count += 1
+
+song1 = "https://www.youtube.com/watch?v=Gp-PKmbcF7c#t=206"
+song2 = "https://www.youtube.com/watch?v=20S_kwNb4rg"
+song3 = "https://www.youtube.com/watch?v=Bayek5lLZWY"
+song_list = [song1, song2, song3]
+take_break(3, song_list)
